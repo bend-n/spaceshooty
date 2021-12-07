aea9a0ecab5eb0b762ef88594cf72a413a66910f
bendnuts <bend.n@outlook.com>
2021-12-08 05:34:42 +0700

    paws menu framework complete

--- pausemenu.gd
+++ pausemenu.gd
@@ -1,16 +1,18 @@
 extends Control
+var focused
 
 func _input(event):
 	if event.is_action_pressed("pause"):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
 		visible = new_pause_state
+		if new_pause_state:
+			$ColorRect/VBoxContainer/mainmenu.grab_focus()
 
-func _on_mainmenu_pressed():
-# warning-ignore:return_value_discarded
-	get_tree().change_scene("res://StartMenu.tscn")
-
-
-# warning-ignore:unused_argument
-func _on_mainmenu_area_entered(area):
-	pass
+func _process(delta):
+	focused = get_focus_owner()
+	if Input.is_action_just_pressed("ui_accept"):
+		if focused == $ColorRect/VBoxContainer/mainmenu:
+			get_tree().change_scene("res://StartMenu.tscn")
+		elif $ColorRect/VBoxContainer/options:
+			pass
